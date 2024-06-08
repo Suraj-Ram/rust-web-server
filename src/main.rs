371e91e05e85b55bd1b653c7f3bbef3ec4172d14
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2024-06-08 19:05:55 +0400

    threads, without limit

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 const SUCCESS_LINE: &str = "HTTP/1.1 200 OK";
 const NOT_FOUND_LINE: &str = "HTTP/1.1 404 NOT FOUND";
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello from web server! Running on port 7878...");
 
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| {
+            println!("spawned thread");
+            handle_connection(stream);
+        });
     }
 }
 
@@ -22,10 +27,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        (SUCCESS_LINE, "hello.html")
-    } else {
-        (NOT_FOUND_LINE, "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => (SUCCESS_LINE, "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            (SUCCESS_LINE, "hello.html")
+        }
+        _ => (NOT_FOUND_LINE, "404.html"),
     };
 
     let html_contents = fs::read_to_string(filename).unwrap();
@@ -33,5 +41,4 @@ fn handle_connection(mut stream: TcpStream) {
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{html_contents}");
     stream.write_all(response.as_bytes()).unwrap();
-
 }
